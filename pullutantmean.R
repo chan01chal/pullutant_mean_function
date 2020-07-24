47635d2b9552bec659c3b4ea2daa07265b09d1a6
Chanchal Meena <52331294+chan01chal@users.noreply.github.com>
2020-07-24 10:53:20 +0530

    pol1
    1st option

---
+++ pullutantmean.R
@@ -0,0 +1,12 @@
+setwd("F:/data_science/courseara R prograamming/specdata")
+pullutantmean <- function(dirctory, pollutant,  ID = 1:332){
+  filelist <- list.files(path = "F:/data_science/courseara R prograamming/specdata", pattern = ".csv", full.names = TRUE)
+values <- numeric()
+
+for (i in ID) {
+  data <- read.csv(filelist[i])
+  values <- c(values, data[[pollutant]])
+}
+  mean(values, na.rm = TRUE)
+}
+pullutantmean("F:/data_science/courseara R prograamming/specdata", "sulfate", 1:2)
